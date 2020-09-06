89e617952093b7cb4cbc111ea242348979f8b3cd
Darlan H <darlanhermessjb@hotmai.com>
2020-09-06 14:08:30 -0300

    init frontend;

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  colors: {
+    text: 'rgb(38, 38, 38)',
+    backgroundColor: 'rgb(250, 250, 250)',
+    primary: 'rgb(38, 38, 38)',
+    seconday: 'rgb(250, 250, 250)'
+  }
+}
+
+export default theme
